40332c222665c7380b19bb86d701d3d7ea19bbdf
abode77 <54987094+abode77@users.noreply.github.com>
2019-10-04 15:55:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`بوت نشر خاص Red knight`,"http://twitch.tv/S-F")
+client.user.setGame(`رمضان وبس`,"http://twitch.tv/S-F")
 client.user.setStatus("Online")
 });
 
